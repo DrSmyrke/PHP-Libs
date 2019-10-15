b81c4f439f89633f91fdc3d1c7c5dc0efe2d1266
Jura Prokofev <Smyrke2005@yandex.ru>
2019-10-15 15:35:00 +0300

    Refactoring lib

--- yaml.php
+++ yaml.php
@@ -1,97 +1,109 @@
 <?php
-function saveYaml($file,$data){
-	$str=parsDataYaml($data,null,0);
-	$fs=fopen($file,"w");
-	if(!fwrite($fs,$str)){fclose($fs);return false;}
-	fclose($fs);
-	return true;
+
+function yaml_help()
+{
+	print "$yaml = new Yaml;<br>\n";
+	print "$yaml->saveToFile( \"1.yml\", $dataArray );<br>\n";
+	print "$dataArray = $yaml->loadFile( \"1.yml\" );<br>\n";
 }
-function parsDataYaml($data,$spc,$recursion){
-	$string="";
-	if($spc==null){$spc=" ";}
-	if($recursion==null){$recursion=0;}
-	foreach($data as $key=>$val){
-		if(is_int($key) and substr($val,0,1)=="#"){
-			for($i=0;$i<$recursion;$i++){$string.="$spc ";}
-			$string.="$val\n";continue;
+
+
+
+class Yaml
+{
+	public function loadFile( $fileName )
+	{
+		$data = array();
+		$prew = &$data;
+		$prewLevel = 0;
+		if( !is_file( $fileName ) ) return $data;
+		$fs = file( $fileName );
+		
+		$keys = array();
+		
+		foreach( $fs as $str ){
+			$str = str_replace( array( "\n", "\r" ), "", $str );
+			
+			$level = 0;
+			while( substr( $str, $level, 1 ) == " " ) $level++;
+			$str = substr( $str, $level );
+			
+			$level /= 2;
+
+			if( strpos( $str, ":" ) !== false ){
+				$tmp = explode( ":", $str );
+				$key = array_shift( $tmp );
+				if( count( $tmp ) > 1 ){
+					$value = join( ":", $tmp );
+				}else{
+					$value = array_shift( $tmp );
+				}
+				if( strlen( $value ) > 1 && substr( $value, 0, 1 ) == " " ) $value = substr( $value, 1 );
+				
+				if( $value == " " || $value == "" ) $value = array();
+				
+				if( $prewLevel > $level ){
+					$r = $prewLevel - $level;
+					for( $i = 0; $i < $r; $i++ ) array_pop( $keys );
+					if( is_array( $value ) ) array_push( $keys, $key );
+				}
+				
+				if( $level > $prewLevel || $level == $prewLevel ){
+					if( is_array( $value ) ) array_push( $keys, $key );
+				}				
+				
+				$prew = &$data;
+				foreach( $keys as $k ){
+					if( isset( $prew[$k] ) ) $prew = &$prew[$k];
+				}
+				$prew[$key] = $value;
+
+				if( $level != $prewLevel ) $prewLevel = $level;
+				continue;
+			}
+			
+			if( substr( $str, 0, 2 ) == "- " ){
+				array_push( $prew[$key], substr( $str, 2 ) );
+				if( $level != $prewLevel ) $prewLevel = $level;
+			}
 		}
-		if(is_int($key) and gettype($val)!="array"){
-			for($i=0;$i<$recursion;$i++){$string.="$spc ";}
-			$string.="- $val\n";continue;
-		}
-		if(gettype($val)=="string" or gettype($val)=="integer"){
-			for($i=0;$i<$recursion;$i++){$string.="$spc ";}
-			$string.="$key: $val\n";continue;
-		}
-		if(gettype($val)=="array"){
-			for($i=0;$i<$recursion;$i++){$string.="$spc ";}
-			$string.="$key:\n";
-			$string.=parsDataYaml($val,$spc,$recursion+1);
+		
+		
+		return $data;
+	}
+	
+	public function saveToFile( $fileName, $data )
+	{
+		if( !is_array( $data ) ) return;
+		$fs = fopen( $fileName, "w" );
+		if( $fs ){
+			fwrite( $fs, Yaml::serializeData( $data ) );
+			fclose( $fs );
 		}
 	}
-	return $string;
-}
-function parsYaml($file){
-	$fs=file($file);
-	$data=array();
-	$prew=null;
-	foreach($fs as $str){
-		$str=str_replace(array("\n","\r"),"",$str);
-		$str=str_replace("  "," ",$str);
-		if(substr($str,0,1)=="#" or $str==""){setComentYaml($data,$str);continue;}
-		$level=0;while(substr($str,$level,1)==" "){$level++;}
-		$str=substr($str,$level);
-		if(substr($str,-1,1)==":"){setArrayYaml($data,$prew,$str,$level);continue;}
-		list($p,$v)=explode(": ",$str);
-		if($p!="" and $v!=""){setPeremYaml($data,$prew,$p,$v,$level);continue;}
-		if(substr($str,0,2)=="- "){setListYaml($data,$prew,$str,$level);continue;}
-	}
-	return $data;
-}
-function parsYamlFromString($string){
-	$fs = explode("\n",$string);
-	$data=array();
-	$prew=null;
-	foreach($fs as $str){
-		$str=str_replace(array("\n","\r"),"",$str);
-		$str=str_replace("  "," ",$str);
-		if(substr($str,0,1)=="#" or $str==""){setComentYaml($data,$str);continue;}
-		$level=0;while(substr($str,$level,1)==" "){$level++;}
-		$str=substr($str,$level);
-		if(substr($str,-1,1)==":"){setArrayYaml($data,$prew,$str,$level);continue;}
-		list($p,$v)=explode(": ",$str);
-		if($p!="" and $v!=""){setPeremYaml($data,$prew,$p,$v,$level);continue;}
-		if(substr($str,0,2)=="- "){setListYaml($data,$prew,$str,$level);continue;}
-	}
-	return $data;
-}
-function setListYaml(&$dt,$pr,$string,$level){
-	$string=substr($string,2);
-	$string=str_replace("\"","'",$string);
-	parsUrlYaml($dt,$pr,$string,"",$level,"list");
-}
-function setPeremYaml(&$dt,&$pr,$p,$v,$level){
-	$tmp=null;for($i=0;$i<$level;$i++){$tmp[]=$pr[$i];}$pr=$tmp;
-	$v=str_replace("\"","'",$v);
-	parsUrlYaml($dt,$pr,$p,$v,$level,"perem");
-}
-function setArrayYaml(&$dt,&$pr,$string,$level){
-	$string=substr($string,0,-1);
-	$tmp=null;for($i=0;$i<$level;$i++){$tmp[]=$pr[$i];}$pr=$tmp;
-	$pr[]=$string;
-}
-function setComentYaml(&$dt,$string){$dt[]=$string;}
-function parsUrlYaml(&$dt,$pr,$per,$val,$level,$type){
-	$tmp="";for($i=0;$i<$level;$i++){$tmp.='["'.$pr[$i].'"]';}
-	switch($type){
-		case "list":
-			eval("\$dt$tmp"."[]=\"$per\";");
-			#print("L [".join("/",$pr)."] \$dt[".($dindex-1)."]$tmp"."[]='$per';<br>");
-			break;
-		case "perem":
-			eval("\$dt$tmp"."[\"$per\"]=\"$val\";");
-			#print("P [".join("/",$pr)."] \$dt$tmp"."[\"$per\"]=\"$val\";<br>");
-			break;
+	
+	public function serializeData( $data, $spc = "  ", $recursion = 0 )
+	{
+		$res = "";
+		if( !is_array( $data ) ) return $res;
+		
+		foreach($data as $key => $val){
+			for( $i = 0; $i < $recursion; $i++ ) $res .= "$spc";
+			$res .= "$key: ";
+			if(gettype($val)=="array"){
+				$res .= "\n";
+				$res .= Yaml::serializeData( $val, $spc, $recursion+1 );
+			}else{
+				if(  is_numeric( $val ) ){
+					$res .= "$val\n";
+				}else{
+					$res .= "\"$val\"\n";
+				}
+			}
+		}
+		
+		return $res;
 	}
 }
+
 ?>
