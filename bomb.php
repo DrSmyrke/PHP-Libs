2fbd4513b3dbdf431e2a6b141d09a1a6c0c5bb73
Jura Prokofev <Smyrke2005@yandex.ru>
2019-10-16 22:37:37 +0300

    Add files via upload

---
+++ bomb.php
@@ -0,0 +1,25 @@
+<?php
+#$agent = $_SERVER['HTTP_USER_AGENT'];
+
+//check for nikto, sql map or "bad" subfolders which only exist on wordpress
+#if (strpos($agent, 'nikto') !== false || strpos($agent, 'sqlmap') !== false || startswith($url,'wp-') || startswith($url,'wordpress') || startswith($url,'wp/') || $_SERVER["REQUEST_URI"] == "/?timetodie")
+#{
+#      sendBomb();
+#      exit();
+#}
+
+function sendBomb(){
+        //prepare the client to recieve GZIP data. This will not be suspicious
+        //since most web servers use GZIP by default
+        header("Content-Encoding: gzip");
+        header("Content-Length: ".filesize('/www/data/10G.gzip'));
+        //Turn off output buffering
+        if (ob_get_level()) ob_end_clean();
+        //send the gzipped file to the client
+        readfile('/www/data/10G.gzip');
+}
+
+function startsWith($a, $b) { 
+    return strpos($a, $b) === 0;
+}
+?>
