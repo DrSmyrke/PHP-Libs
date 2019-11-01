71e96d25243be97d280ef27477c86ac741097c04
Jura Prokofev <Smyrke2005@yandex.ru>
2019-11-01 12:54:34 +0300

    Update vkBot.php

--- vkBot.php
+++ vkBot.php
@@ -2,14 +2,129 @@
 
 function vkBot_help()
 {
-	#print "<br>\n";
+	print "$vkBot = new vkBot();<br>\n";
+	print "$vkBot->setConfirmationToken( CALLBACK_API_CONFIRMATION_TOKEN );<br>\n";
+	print "$vkBot->setGroupID( CALLBACK_API_GROUP_ID )<br>\n";
+	print "$vkBot->setAccessToken( VK_API_ACCESS_TOKEN )<br>\n";
+	print "$vkBot->init()<br>\n";
+	print "$data = $vkBot->execute();<br>\n";
+	print "$color = $vkBot->replaceColor( \"blue\" );<br>\n";
 }
 
 
 
 class vkBot
 {
+	private $confirmationToken		= "";
+	private $groupID				= "";
+	private $accessToken			= "";
 	
+	public function init()
+	{
+		define('VK_API_VERSION', '5.67');
+		define('VK_API_ENDPOINT', 'https://api.vk.com/method/');
+	}
+	
+	public function execute()
+	{
+		$returnData = array();
+		
+		$requestData = vkBot::getData();
+		
+		
+		$group_id = ( isset( $requestData["group_id"] ) ) ? $requestData["group_id"] : "";
+		$type = ( isset( $requestData["type"] ) ) ? $requestData["type"] : "";
+		$user_id = ( isset( $requestData["object"]["user_id"] ) ) ? $requestData["object"]["user_id"] : "";
+		
+		$returnData["type"] = $type;
+		$returnData["user_id"] = $user_id;
+		
+		switch( $type ){
+			case "confirmation":
+				if( $group_id == $this->groupID ){
+					print $this->confirmationToken;
+				}else{
+					print "ERROR";
+				}
+				return $returnData;
+			break;
+			case "message_new":
+				if( isset( $requestData["object"]["body"] ) ){
+					$returnData["mess"] = $requestData["object"]['body'];
+				}
+			break;
+		}
+		
+		
+		
+		return $returnData;
+	}
+	
+	public function sendMessage($targetID, $message, $attachments = array())
+	{
+		return vkBot::call('messages.send', array(
+			'peer_id'		=> $targetID,
+			'message'		=> $message,
+			'attachment'	=> implode(',', $attachments)
+		));
+	}
+	
+	public function sendButtons($targetID, $message, $buttons = array(), $one_time = true)
+	{
+		$buttonsSend = array(
+            "one_time" => $one_time,
+            "buttons" => $buttons
+		);
+		
+		$this->sendMessage( $targetID, $message );
+		
+		return vkBot::call('messages.send', array(
+			'peer_id'		=> $targetID,
+			'message'		=> $message,
+			'keyboard'		=> json_encode($buttonsSend, JSON_UNESCAPED_UNICODE)
+		));
+	}
+	
+	private function call($method, $params = array())
+	{
+		$params['access_token'] = $this->accessToken;
+		$params['v'] = VK_API_VERSION;
+
+		$query = http_build_query( $params );
+		$url = VK_API_ENDPOINT.$method.'?'.$query;
+
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$json = curl_exec($curl);
+		$error = curl_error($curl);
+		if ($error) return "Failed '{$method}' request";
+
+		curl_close($curl);
+
+		$response = json_decode($json, true);
+		if (!$response || !isset($response['response'])) return "Invalid response for '{$method}' request";
+
+		return $response['response'];
+	}
+
+	public function setConfirmationToken( $value ){ $this->confirmationToken = $value; }
+	public function setGroupID( $value ){ $this->groupID = $value; }
+	public function setAccessToken( $value ){ $this->accessToken = $value; }
+	
+	private function getData(){ return json_decode(file_get_contents('php://input'), true); }
+	
+	public function replaceColor($color)
+	{
+		switch ($color) {
+			case 'red':		$color = 'negative';	break;
+			case 'green':	$color = 'positive';	break;
+			case 'white':	$color = 'default';		break;
+			case 'blue':	$color = 'primary';		break;
+
+			default: break;
+        }
+        return $color;
+    }
 }
 
 ?>
