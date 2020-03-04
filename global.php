b8ec4b8ff855f90fdc70f9cfeba5c4aedeff9337
Jura Prokofev <Smyrke2005@yandex.ru>
2020-03-04 22:59:21 +0300

    Add files via upload

--- global.php
+++ global.php
@@ -11,14 +11,16 @@
 	$thisPage		= baseName( $_SERVER["SCRIPT_NAME"] );
 	
 	######################################################
-
-	if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
-		if( isset( $_POST["cmd"] ) ) $cmd = $_POST["cmd"];
-		if( isset( $_POST["data"] ) ) $data = $_POST["data"];
-	}
-	if( $_SERVER['REQUEST_METHOD'] == 'GET' ){
-		if( isset( $_GET["cmd"] ) ) $cmd = $_GET["cmd"];
-		if( isset( $_GET["data"] ) ) $data = $_GET["data"];
+	
+	if( isset( $_SERVER['REQUEST_METHOD'] ) ){
+		if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
+			if( isset( $_POST["cmd"] ) ) $cmd = $_POST["cmd"];
+			if( isset( $_POST["data"] ) ) $data = $_POST["data"];
+		}
+		if( $_SERVER['REQUEST_METHOD'] == 'GET' ){
+			if( isset( $_GET["cmd"] ) ) $cmd = $_GET["cmd"];
+			if( isset( $_GET["data"] ) ) $data = $_GET["data"];
+		}
 	}
 	
 	######################################################
