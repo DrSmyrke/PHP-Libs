41b07598fdecbbfcc9520b7b50245c65140be046
DrSmyrke <Smyrke2005@yandex.ru>
2020-03-12 22:14:53 +0300

    Update MyEngine.php

--- MyEngine.php
+++ MyEngine.php
@@ -64,6 +64,7 @@ function chkCookie()
 function acceptCookie()
 {
 	setCookie( "acceptCookie", true );
+	document.location.reload();
 }
 '."\n";
 
@@ -78,6 +79,24 @@ function acceptCookie()
 			$language = $_COOKIE["lang"];
 		}
 		*/
+		if( !is_dir( "data/img" ) ) mkdir( "data/img", 755, true );
+		if( !is_dir( "data/img/sites_icons" ) ) mkdir( "data/img/sites_icons", 755, true );
+		if( is_dir( "data/img" ) ){
+			if( !is_file( "data/img/my.png" ) ) copy( "/my/www/html/data/img/my.png", "data/img/my.png" );
+			if( !is_file( "data/img/fon.png" ) ) copy( "/my/www/html/data/img/fon.png", "data/img/fon.png" );
+			if( !is_file( "data/img/line.png" ) ) copy( "/my/www/html/data/img/line.png", "data/img/line.png" );
+		}
+		if( is_dir( "data/img/sites_icons" ) ){
+			if( !is_file( "data/img/sites_icons/hh_ru.svg" ) ) copy( "/my/www/html/data/img/sites_icons/hh_ru.svg", "data/img/sites_icons/hh_ru.svg" );
+			if( !is_file( "data/img/sites_icons/icq.png" ) ) copy( "/my/www/html/data/img/sites_icons/icq.png", "data/img/sites_icons/icq.png" );
+			if( !is_file( "data/img/sites_icons/skype.png" ) ) copy( "/my/www/html/data/img/sites_icons/skype.png", "data/img/sites_icons/skype.png" );
+			if( !is_file( "data/img/sites_icons/stihi.svg" ) ) copy( "/my/www/html/data/img/sites_icons/stihi.svg", "data/img/sites_icons/stihi.svg" );
+			if( !is_file( "data/img/sites_icons/SuperJob.png" ) ) copy( "/my/www/html/data/img/sites_icons/SuperJob.png", "data/img/sites_icons/SuperJob.png" );
+			if( !is_file( "data/img/sites_icons/thingiverse.png" ) ) copy( "/my/www/html/data/img/sites_icons/thingiverse.png", "data/img/sites_icons/thingiverse.png" );
+			if( !is_file( "data/img/sites_icons/vk.png" ) ) copy( "/my/www/html/data/img/sites_icons/vk.png", "data/img/sites_icons/vk.png" );
+			if( !is_file( "data/img/sites_icons/youtube.png" ) ) copy( "/my/www/html/data/img/sites_icons/youtube.png", "data/img/sites_icons/youtube.png" );
+			if( !is_file( "data/img/sites_icons/cults-3d.svg" ) ) copy( "/my/www/html/data/img/sites_icons/cults-3d.svg", "data/img/sites_icons/cults-3d.svg" );
+		}
 	}
 
 	public function getLanguage(){ return $this->language; }
@@ -161,6 +180,7 @@ function acceptCookie()
 			<a href="https://github.com/DrSmyrke" target="_blank" class="button"><svg aria-hidden="true" title="GitHub" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
 			<a href="https://www.stihi.ru/avtor/drsmyrke" target="_blank" class="button"><img src="/data/img/sites_icons/stihi.svg" title="Стихи.ру"></a>
 			<a href="https://www.thingiverse.com/DrSmyrke" target="_blank" class="button"><img src="/data/img/sites_icons/thingiverse.png" title="Thingiverse"></a>
+			<a href="https://cults3d.com/en/users/DrSmyrke/creations" target="_blank" class="button"><img src="/data/img/sites_icons/cults-3d.svg" title="Cults 3d"></a>
 		</div>
 	</div>
 	<hr>
