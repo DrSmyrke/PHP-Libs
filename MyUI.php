aaf2efd7f822ff26418aded3b2e3ade7ba21b780
Jura Prokofev <Smyrke2005@yandex.ru>
2020-04-14 14:56:31 +0300

    added printProgressBar function

--- MyUI.php
+++ MyUI.php
@@ -10,5 +10,16 @@ class MyUI
 	{
 		
 	}
+
+	public function printProgressBar( $prz, $text = "", $bradius = 0, $bg = "", $fg = "orange, orange", $space = true )
+	{
+		if( $text == "" ) $text = "{PRZ}%";
+		$text = str_replace( "{PRZ}", $prz, $text );
+		$text = str_replace( "{UNPRZ}", ( 100 - $prz ), $text );
+		$background = ( $bg != "" ) ? 'linear-gradient(to bottom, '.$bg.')' : 'rgba( 0, 0, 0, 0 )';
+		$pbarground = ( $fg != "" ) ? 'linear-gradient(to bottom, '.$fg.') no-repeat' : '';
+		if( $space ) $pbarground .= " content-box";
+		print '<div style="border-radius: '.$bradius.'px;border:1px solid silver;width:100%;padding:3px;font-size:9pt;background:'.$pbarground.', '.$background.';background-size:'.$prz.'% 100%, 100% 100%;">'.$text.'</div>';
+	}
 }
 ?>
