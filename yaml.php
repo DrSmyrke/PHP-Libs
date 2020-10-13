6e2cfbd662255a8dc4d0264affa33fe036ea620c
DrSmyrke <Smyrke2005@yandex.ru>
2020-10-14 00:11:01 +0300

    Update yaml.php: Bug fix

--- yaml.php
+++ yaml.php
@@ -18,25 +18,26 @@ class Yaml
 		$prewLevel = 0;
 		if( !is_file( $fileName ) ) return $data;
 		$fs = file( $fileName );
-		
+
 		$keys = array();
-		
+
 		foreach( $fs as $str ){
 			$str = str_replace( array( "\n", "\r" ), "", $str );
-			
+
 			$level = 0;
 			$firstSym = substr( $str, 0, 1 );
 			if( $firstSym == " " || $firstSym == "	" ){
 				if( $firstSym == " " ){
 					while( substr( $str, $level, 1 ) == " " ) $level++;
+					$level /= 2;
 				}
 				if( $firstSym == "	" ){
 					while( substr( $str, $level, 1 ) == "	" ) $level++;
 				}
 				$str = substr( $str, $level );
 			}
-			
-			$level /= 2;
+
+
 
 			if( strpos( $str, ":" ) !== false ){
 				$tmp = explode( ":", $str );
@@ -47,24 +48,24 @@ class Yaml
 					$value = array_shift( $tmp );
 				}
 				if( strlen( $value ) > 1 && substr( $value, 0, 1 ) == " " ) $value = substr( $value, 1 );
-				
+
 				if( $value == " " || $value == "" ) $value = array();
-				
+
 				if( $prewLevel > $level ){
 					$r = $prewLevel - $level;
 					for( $i = 0; $i < $r; $i++ ) array_pop( $keys );
 					if( is_array( $value ) ) array_push( $keys, $key );
 				}
-				
+
 				if( $level > $prewLevel || $level == $prewLevel ){
 					if( is_array( $value ) ) array_push( $keys, $key );
-				}				
-				
+				}
+
 				$prew = &$data;
 				foreach( $keys as $k ){
 					if( isset( $prew[$k] ) ) $prew = &$prew[$k];
 				}
-				
+
 				if( !is_array( $value ) ) $value = str_replace( "\"", "", $value );
 				if( is_string( $value ) ){
 					$lowerValue = strtolower( $value );
@@ -76,17 +77,17 @@ class Yaml
 				if( $level != $prewLevel ) $prewLevel = $level;
 				continue;
 			}
-			
+
 			if( substr( $str, 0, 2 ) == "- " ){
 				array_push( $prew[$key], substr( $str, 2 ) );
 				if( $level != $prewLevel ) $prewLevel = $level;
 			}
 		}
-		
-		
+
+
 		return $data;
 	}
-	
+
 	public function saveToFile( $fileName, $data )
 	{
 		if( !is_array( $data ) ) return;
@@ -96,12 +97,12 @@ class Yaml
 			fclose( $fs );
 		}
 	}
-	
+
 	public function serializeData( $data, $spc = "  ", $recursion = 0 )
 	{
 		$res = "";
 		if( !is_array( $data ) ) return $res;
-		
+
 		foreach($data as $key => $val){
 			for( $i = 0; $i < $recursion; $i++ ) $res .= "$spc";
 			$res .= "$key: ";
@@ -116,9 +117,10 @@ class Yaml
 				}
 			}
 		}
-		
+
 		return $res;
 	}
 }
 
 ?>
+
