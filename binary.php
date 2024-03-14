34ba2380a3b3f89020e9494b4a1ae48b10ed6924
DrSmyrke <smyrke2005@yandex.ru>
2024-03-14 19:15:20 +0530

    Added new library

---
+++ binary.php
@@ -0,0 +1,93 @@
+<?php
+function Binary_help()
+{
+	// print "\$binary = new Binary;<br>\n";
+}
+
+class Binary
+{
+	private $success				= false;
+
+	/**
+	 * Packing value to unsigned 8 bit value
+	 * 
+	 * @param integer value
+	 * @param boolean value
+	 * @return packed binary string
+	 */
+	static public function packToU8( $value, $to0xString = false )
+	{
+		$binarydata = pack( 'C*', $value );
+		return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+	}
+
+	static public function packToU16( $value, $to0xString = false )
+	{
+		$binarydata = pack( 'n*', $value );
+		return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+	}
+
+	static public function packToU32( $value, $to0xString = false )
+	{
+		$binarydata = pack( 'N*', $value );
+		return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+	}
+
+	static public function unpackToU8Array( &$inData, &$outData )
+	{
+		$outData = unpack( 'C*', $inData );
+	}
+
+	static public function unpackToU16Array( &$inData, &$outData )
+	{
+		$outData = unpack( 'n*', $inData );
+	}
+
+	static public function unpackToU32Array( &$inData, &$outData )
+	{
+		$outData = unpack( 'N*', $inData );
+	}
+
+	static public function packArrayU8( $data, $to0xString = false )
+	{
+		$binarydata = NULL;
+
+		if( !is_array( $data ) ) return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+		foreach( $data as $value ){
+			if( $value == '' || $value == NULL ) continue;
+			$binarydata .= Binary::packToU8( $value );
+		}
+
+		return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+	}
+
+	//-------------------------------------------------------------
+	static public function packArrayU16( $data, $to0xString = false )
+	{
+		$binarydata = NULL;
+
+		if( !is_array( $data ) ) return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+		foreach( $data as $value ){
+			if( $value == '' || $value == NULL ) continue;
+			$binarydata .= Binary::packToU16( $value );
+		}
+
+		return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+	}
+
+	//-------------------------------------------------------------
+	static public function packArrayU32( $data, $to0xString = false )
+	{
+		$binarydata = NULL;
+
+		if( !is_array( $data ) ) return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+		foreach( $data as $value ){
+			if( $value == '' || $value == NULL ) continue;
+			$binarydata .= Binary::packToU32( $value );
+		}
+
+		
+		return ( $to0xString ) ? '0x'.bin2hex( $binarydata ) : $binarydata;
+	}
+}
+?>
